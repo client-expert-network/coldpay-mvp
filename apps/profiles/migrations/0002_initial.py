b5b2ef73624a89ca18e4c53d37be9eee2fdd9ce9
Tobias Kim <madeffort0903@gmail.com>
2024-07-17 13:58:28 +0900

    유저네임 중복없는 한글로 변경

--- apps/profiles/migrations/0002_initial.py
+++ apps/profiles/migrations/0002_initial.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('calendars', '0001_initial'),
+        ('profiles', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='event',
-            name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            model_name='profile',
+            name='expert',
+            field=models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
